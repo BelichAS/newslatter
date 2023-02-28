6f73c113396afa45fa7544d80843e69a9de3192d
BelichAS <obzornikov@gmail.com>
2023-02-28 14:30:16 +1000

    second test from desktop

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.post("/", function (req, res) {
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html")
+      console.log("Success")
     } else {
       res.sendFile(__dirname + "/failure.html")
       console.log("Fail")
